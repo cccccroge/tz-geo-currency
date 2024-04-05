60981de6b5c034d51771792d9163a483c9b85f0d
Allen He <pooopooo543@gmail.com>
2024-04-06 01:54:37 +0800

    Core API done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
+import { countryToCurrencies } from "./mappings/countryToCurrencies";
+import { timezoneToCountries } from "./mappings/timezoneToCountries";
+
 // TL namespace, stands for `TimeLocate`
 const TL = {};
 
-TL.getCountry = function() {
+/**
+ * Return a country alpha2 that match user's timezone. 
+ * If there are multiple countries, then the first one in the dataset is returned.
+ */
+
+TL.getCountry = function () {
   const tz = getTimezone();
-  return timezoneToCountry(tz);
+  const countries = timezoneToCountries(tz);
+
+  if (!countries.length) {
+    return null;
+  }
+
+  return countries[0];
 }
 
-TL.getCurrency = function() {
+/**
+ * Return an array of possible countries in alpha2 that match user's timezone.
+ */
+TL.getCountries = function() {
+  const tz = getTimezone();
+  return timezoneToCountries(tz);
+}
+
+/**
+ * Get the corresponding currencies in ISO-4217 that return from `getCountry`
+ */
+TL.getCurrencies = function() {
   const country = TL.getCountry();
-  return countryToCurrency(country);
+
+  if (!country) {
+    return [];
+  }
+
+  return countryToCurrencies(country);
+}
+
+/**
+ * Get the corresponding currencies in ISO-4217 of the specified country
+ */
+TL.getCurrenciesFromCountry = function(countryCode) {
+  return countryToCurrencies(countryCode);
 }
 
 function getTimezone() {
---
+++ src/mappings/countryToCurrencies.js
@@ -0,0 +1,7 @@
+import countryData from '../data/parsedCountries';
+
+export function countryToCurrencies(countryCode) {
+  const country = countryData.find(country => country.cca2 === countryCode);
+
+  return country ? country.currencies : [];
+}
